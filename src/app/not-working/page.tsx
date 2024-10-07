c5984f365cb87c337ba5f6ee9196b78a8c9fdeba
n1xx1 <me@simonemiraglia.it>
2024-10-07 10:30:16 +0200

    split client server page

--- src/app/not-working/page.tsx
+++ src/app/not-working/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-
-import { CalendarIcon } from "@chakra-ui/icons";
-import { Button, Container, Stack } from "@chakra-ui/react";
-import Link from "next/link";
+import { ClientPage } from "./client";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return (
-    <Container py="10">
-      <Stack>
-        <Button leftIcon={<CalendarIcon />}>Example</Button>
-        <Button as={Link} href="/working">
-          Go to Working
-        </Button>
-        <Button as="a" href="/not-working">
-          Hard Refresh
-        </Button>
-      </Stack>
-    </Container>
-  );
+  return <ClientPage />;
 }
--- src/app/working/page.tsx
+++ src/app/working/page.tsx
@@ -1,22 +1,7 @@
-"use client";
-
-import { Button, Container, Stack } from "@chakra-ui/react";
-import Link from "next/link";
+import { ClientPage } from "./client";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return (
-    <Container py="10">
-      <Stack>
-        <Button>Example</Button>
-        <Button as={Link} href="/not-working">
-          Go to Not Working
-        </Button>
-        <Button as="a" href="/working">
-          Hard Refresh
-        </Button>
-      </Stack>
-    </Container>
-  );
+  return <ClientPage />;
 }
